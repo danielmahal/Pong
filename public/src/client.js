079e4433ff7cb56bb1d3b4ffc649f86bb90e100e
Daniel <danielmahal@gmail.com>
2013-10-01 00:16:26 +0200

    Simple matchmaking

--- public/src/client.js
+++ public/src/client.js
@@ -5,6 +5,7 @@ var canvas = document.getElementById('canvas');
 var context = canvas.getContext('2d');
 var w;
 var game = new Game();
+var animationRequest;
 
 var updateCanvasSize = function() {
     w = { x: window.innerWidth, y: window.innerHeight };
@@ -31,10 +32,8 @@ var draw = function() {
 var loop = function() {
     game.update();
     draw();
-    requestAnimationFrame(loop);
+    animationRequest = requestAnimationFrame(loop);
 }
-
-loop();
 
 socket.on('connect', function (data) {
     console.log('Connected to socket');
@@ -44,4 +43,20 @@ socket.on('ball:update', function(ball) {
     for(var i in ball) {
         game.ball[i] = ball[i];
     }
+});
+
+socket.on('game:start', function() {
+    console.log('Starting game');
+    game = new Game();
+    loop();
+});
+
+socket.on('opponent:left', function() {
+    console.log('Opponent left');
+    context.clearRect(0, 0, w.x, w.y);
+    cancelAnimationFrame(animationRequest);
+});
+
+socket.on('matchmaking', function() {
+    console.log('Waiting for opponent');
 });
--- server.js
+++ server.js
@@ -1,32 +1,63 @@
 var express = require('express'),
     http = require('http'),
     socketio = require('socket.io'),
-    Game = require('./src/Game');
+    Game = require('./src/Game'),
+    crypto = require('crypto');
 
 var app = express();
 var server = http.createServer(app);
-var io = socketio.listen(server);
+var io = socketio.listen(server, {
+    log: false
+});
 
 server.listen(8000);
 
 app.use(express.static(__dirname + '/public'));
 
 io.sockets.on('connection', function (socket) {
+    matchmake(socket);
+});
+
+var matchmake = function(socket) {
+    var lobby = io.sockets.clients('lobby');
+
+    if(lobby.length) {
+        createGame([socket, lobby[0]]);
+    } else {
+        socket.join('lobby');
+        socket.emit('matchmaking');
+    }
+}
+
+var createGame = function(sockets) {
+    var id = crypto.randomBytes(20).toString('hex');
+
     var game = new Game();
-
-    game.ball.on('update', function() {
-        socket.emit('ball:update', game.ball);
-    });
-
-    socket.emit('game:start', game);
 
     var interval = setInterval(function() {
         game.update();
     }, 1000 / 60);
 
-    socket.on('disconnect', function() {
-        clearInterval(interval);
-        // clearInterval(sendInterval);
-        console.log('Client disconnected');
-    })
-});
+    game.ball.on('update', function() {
+        io.sockets.in(id).emit('ball:update', game.ball);
+    });
+
+    sockets.forEach(function(socket) {
+        socket.leave('lobby');
+        socket.join(id);
+
+        socket.on('disconnect', function() {
+            clearInterval(interval);
+
+            io.sockets.clients(id).forEach(function(s, i) {
+                if(s == socket) return;
+
+                s.emit('opponent:left');
+                s.leave(id);
+                matchmake(s);
+            });
+        });
+    });
+
+    io.sockets.in(id).emit('game:start');
+}
